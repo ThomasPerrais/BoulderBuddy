e6e6ed6b6df43fee46e9ba7a015c3b11d6a8dae4
Thomas Perrais <thomas_30_@hotmail.fr>
2023-06-02 15:38:01 +0200

    Problem extra methods for display in Gallery mode

--- gymstats/templatetags/pb_extras.py
+++ gymstats/templatetags/pb_extras.py
@@ -10,3 +10,24 @@ def problem_title(pb: Problem):
     Display function used as title for a Problem
     """
     return "{} - {} {} ({})".format(pb.grade, pb.gym.brand, pb.gym.city, pb.date_added.strftime("%d/%m/%y"))
+
+
+@register.filter(name="gallery_display")
+def problem_gallery_title(pb: Problem):
+    return "{} - {}".format(pb.grade, pb.sector);
+
+
+@register.filter(name="fw")
+def problem_fw(pb: Problem):
+    for fw in pb.footwork.all():
+        yield fw
+
+@register.filter(name="hh")
+def problem_hh(pb: Problem):
+    for hh in pb.hand_holds.all():
+        yield hh
+
+@register.filter(name="me")
+def problem_hh(pb: Problem):
+    for me in pb.problem_method.all():
+        yield me
