0ceac6968d26755cb985dda64f8e2f941de15721
Thomas Perrais <thomas_30_@hotmail.fr>
2023-08-21 19:31:00 +0200

    add grade order for Boulder Line. Fix with default value

--- gymstats/helper/grade_order.py
+++ gymstats/helper/grade_order.py
@@ -4,9 +4,11 @@ GRADE_ORDER = {
     "va": ["white", "orange", "green", "blue", "red", "black", "purple"],
     "bo": ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10", "b11", "b12", "b13", "b14"],
     "bs": ["blue", "green", "orange", "pink", "black", "gray", "white"],
+    "bl": ["yellow", "orange", "blue", "red", "green", "black"]
 }
 
 BRAND_TO_ABV = {
+    "Boulder Line": "bl",
     "Climbing District": "cd",
     "Vertical'Art": "va",
     "Block'Out": "bo",
@@ -19,5 +21,5 @@ def grades_list(gym, default=True):
     if gym.brand in BRAND_TO_ABV:
         values = GRADE_ORDER[BRAND_TO_ABV[gym.brand]]
     elif default:
-        values = GRADE_ORDER[BRAND_TO_ABV["@default"]]
+        values = GRADE_ORDER["@default"]
     return [elt[0].upper() + elt[1:] for elt in values]
